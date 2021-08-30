f8e0b15634731c7224c5dd183202f24e5e344046
Marcio <marciodevelopment@gmail.com>
2021-08-30 13:39:44 -0300

    first commit

---
+++ codes/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+
+	w.Write([]byte("<h1> Hello full cycle!!! </h1>"))
+
+}
